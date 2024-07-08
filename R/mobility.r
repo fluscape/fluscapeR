707d560955683eb901c068e8234db373d385aa13
VVVVivi <haowei.wang18@imperial.ac.uk>
2024-07-08 09:53:12 +0100

    update code

--- R/mobility.r
+++ R/mobility.r
@@ -2,9 +2,7 @@
 
 generate.agg.Smatrix <- function(contacts,
                                  raster_object,
-                                 agg_num,
-                                 #input: gravity or radiation
-                                 model_name
+                                 agg_num
                                  ){
   # remove NA values of longitude and latitude
   contacts_small <- contacts[!is.na(contacts$long) | !is.na(contacts$lat) | !is.na(contacts$HH_Long) | !is.na(contacts$HH_Lat),]
@@ -53,50 +51,23 @@ generate.agg.Smatrix <- function(contacts,
   }
   close(pb)
 
-  # create S matrix for gravity or radiation model respectively according to the input
-  if (model_name == "gravity"){
-    # faster gravity model generation
-    M <- 1
-    # Make a matrix of predicted origin-destination weights for gravity model
-    #   with an offset of 500 and a power term M.
-    S <- matrix(nrow=noorig, ncol=nodest )
-    pb <- txtProgressBar(min = 0, max = noorig, initial = 0)
-    for (i in 1:noorig) {
-      orcell 	<- originindices$cell[i]
-      dscell 	<- destindices$cell
-      n_o 	<- raster::extract(x2,orcell)
-      n_d 	<- raster::extract(x2,dscell)
-      dist 	<- distances[i, ]
-      S[i, ] <- (n_o * n_d) / (500 + dist^M)
-      setTxtProgressBar(pb,i)
+  # Find radiation density sums, s_ij, for each orig-dest cell combination.
+  # Finds the total population density within equivalent distance from the origin.
+  # Warning, this is slow and takes a long time.
+  # Save to disk after generation for future use.
+  S <- matrix(nrow=noorig, ncol=nodest )
+  for (i in 1:noorig) {
+    print(paste(i,"/",noorig))
+    dist <- distances[i, ]
+    pb <- txtProgressBar(min = 0, max = nodest, style = 3)
+    for (j in 1:nodest) {
+      within_d <- which(dist<=dist[j])
+      radcell <- destindices$cell[within_d] # all destination cells within radius dist
+      S[i,j] <- sum(raster::extract(x,radcell) )
+      setTxtProgressBar(pb, j )
+      close(pb)
     }
-    close(pb)
-    pb <- txtProgressBar(min = 0, max = noorig, initial = 0)
-    for (i in 1:noorig) {
-      S[i,] <- S[i,] / sum(S[i,])
-      setTxtProgressBar(pb,i)
-    }
-    close(pb)
   }
 
-  if (model_name == "radiation"){
-    # Find radiation density sums, s_ij, for each orig-dest cell combination.
-    # Finds the total population density within equivalent distance from the origin.
-    # Warning, this is slow and takes a long time.
-    # Save to disk after generation for future use.
-    S <- matrix(nrow=noorig, ncol=nodest )
-    for (i in 1:noorig) {
-      print(paste(i,"/",noorig))
-      dist <- distances[i, ]
-      pb <- txtProgressBar(min = 0, max = nodest, style = 3)
-      for (j in 1:nodest) {
-        within_d <- which(dist<=dist[j])
-        radcell <- destindices$cell[within_d] # all destination cells within radius dist
-        S[i,j] <- sum(raster::extract(x,radcell) )
-        setTxtProgressBar(pb, j )
-        close(pb)
-      }
-    }
-  }
   return (S)
 }
